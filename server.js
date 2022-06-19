ba006278cdd1fc0e519596273c31e9bd823a0066
enjoyin enjoyin <stefandrewitzyo@gmail.com>
2022-06-20 00:58:34 +0200

    fixed cors setting

--- server.js
+++ server.js
@@ -5,7 +5,12 @@ const cookieparser = require('cookie-parser');
 const app = express();
 
 app.use(express.json());
-app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
+app.use(
+  cors({
+    credentials: true,
+    origin: 'https://gregarious-cocada-c96629.netlify.app/',
+  })
+);
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieparser());
 app.use('/api/v1', router);
